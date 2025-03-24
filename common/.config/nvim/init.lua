b19eee827610d1518ddedb53d02be6a0855070a6
xfan <serfinxx@gmail.com>
2025-03-25 00:17:46 +0800

    refactor

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -96,7 +96,7 @@ if not vim.loop.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
   vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
 end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
+vim.opt.runtimepath:prepend(lazypath)
 
 -- [[ Configure and install plugins ]]
 require('lazy').setup {
