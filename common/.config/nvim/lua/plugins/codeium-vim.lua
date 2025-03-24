2328b18292d232ec151b205d50cd45cb05baa4dd
xfan <serfinxx@gmail.com>
2025-03-24 23:35:48 +0800

    fix: disable tab for accept

--- common/.config/nvim/lua/plugins/codeium-vim.lua
+++ common/.config/nvim/lua/plugins/codeium-vim.lua
@@ -4,15 +4,16 @@ return { -- Codeium integration
   config = function()
     vim.keymap.set('i', '<C-a>', function()
       return vim.fn['codeium#Accept']()
-    end, { expr = true, silent = true })
+    end, { expr = true, silent = true, desc = 'Codeium Accept' })
     vim.keymap.set('i', '<C-x>', function()
       return vim.fn['codeium#Clear']()
-    end, { expr = true, silent = true })
+    end, { expr = true, silent = true, desc = 'Codeium Clear' })
     vim.keymap.set('i', '<C-,>', function()
       return vim.fn['codeium#CycleCompletions'](1)
-    end, { expr = true, silent = true })
+    end, { expr = true, silent = true, desc = 'Codeium Next Suggestion' })
     vim.keymap.set('i', '<C-.>', function()
       return vim.fn['codeium#CycleCompletions'](-1)
-    end, { expr = true, silent = true })
+    end, { expr = true, silent = true, desc = 'Codeium Previous Suggestion' })
+    vim.g.codeium_no_map_tab = 1
   end,
 }
