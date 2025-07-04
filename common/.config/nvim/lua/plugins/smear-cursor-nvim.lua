604a4a128f4cffdaedea0b854e9202f66709fb85
Xuan-Rui <serfinxx@gmail.com>
2025-07-04 17:19:07 +0800

    chore: add animation

---
+++ common/.config/nvim/lua/plugins/smear-cursor-nvim.lua
@@ -0,0 +1,12 @@
+return { -- Animate cursor
+  'sphamba/smear-cursor.nvim',
+  opts = {
+    stiffness = 0.8,
+    trailing_stiffness = 0.5,
+    stiffness_insert_mode = 0.7,
+    trailing_stiffness_insert_mode = 0.7,
+    damping = 0.8,
+    damping_insert_mode = 0.8,
+    distance_stop_animating = 0.5,
+  },
+}
